b3f6b79d350bd0b783c1e6eaeb37bd201348b6f7
liyizhe <liyizhe@yidian-inc.com>
2019-04-16 18:30:55 +0800

    add bestTimeToBuyAndSellStockII

---
+++ bestTimeToBuyAndSellStockII.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let i = 0, valley = prices[0], peak = prices[0], maxProfit = 0;
+    while(i < prices.length - 1) {
+        while(i < prices.length - 1 && prices[i] >= prices[i+1]) {
+            i++;
+        }
+        valley = prices[i];
+        while(i < prices.length - 1 && prices[i] <= prices[i+1]) {
+            i++;
+        }
+        peak = prices[i];
+        maxProfit +=  peak - valley;
+    }
+    return maxProfit;
+};
+
+module.exports = maxProfit;
---
+++ bestTimeToBuyAndSellStockII.test.js
@@ -0,0 +1,5 @@
+const maxProfit = require('./bestTimeToBuyAndSellStockII');
+
+test('max profit of [7,1,5,3,6,4] is 7', () => {
+  expect(maxProfit([7,1,5,3,6,4])).toBe(7);
+})
