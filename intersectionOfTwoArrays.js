e85e840a2fdd7bf2a82d895ae2f933f2e1161af0
buptlyz <liyizhe@bupt.edu.cn>
2019-04-18 23:27:17 +0800

    add intersectionOfTwoArrays

---
+++ intersectionOfTwoArrays.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+  nums1 = nums1.sort();
+  nums2 = nums2.sort();
+  const len = Math.max(nums1.length, nums2.length),
+    res = new Set();
+  let i = 0,
+    j = 0;
+  while (i < len && j < len) {
+    if (nums1[i] === nums2[j]) {
+      res.add(nums1[i]);
+      i++;
+      j++;
+    } else if (nums1[i] < nums2[j]) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+  return [...res];
+};
+
+module.exports = intersect;
---
+++ intersectionOfTwoArrays.test.js
@@ -0,0 +1,8 @@
+const intersect = require('./intersectionOfTwoArrays');
+
+test('intersection of [1, 2, 2, 1] and [2, 2] is [2]', () => {
+  expect(intersect([1, 2, 2, 1], [2, 2])).toEqual([2]);
+})
+test('intersection of [4,9,5] and [9,4,9,8,4] is [4,9]', () => {
+  expect(intersect([4,9,5], [9,4,9,8,4])).toEqual([4,9]);
+})
