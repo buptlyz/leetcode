cd6a1a2e5dedc9fd95eae5ee23b08bce883eaec0
buptlyz <liyizhe@bupt.edu.cn>
2019-05-23 09:09:40 +0800

    add comment

--- utils/mergeSort.js
+++ utils/mergeSort.js
@@ -19,7 +19,10 @@ function merge(A, p, q, r, sortType) {
 }
 
 function mergeAscend(A, p, q, r) {
-  const n1 = q - p + 1, n2 = r - q, L = [], R = [];
+  const n1 = q - p + 1, // 左子数组length
+        n2 = r - q, // 右子数组length
+        L = [], // 左子数组
+        R = []; // 右子数组
   let i, j;
   for (i = 0; i < n1; i++) {
     L[i] = A[p + i];
@@ -27,10 +30,10 @@ function mergeAscend(A, p, q, r) {
   for (j = 0; j < n2; j++) {
     R[j] = A[q + 1 + j];
   }
-  L[n1] = Infinity;
+  L[n1] = Infinity; // 尾部添加大数，作为标识位
   R[n2] = Infinity;
   i = j = 0;
-  for (let k = p; k <= r; k++) {
+  for (let k = p; k <= r; k++) { // 对比左右子数组顶部元素，取较小值
     if (L[i] <= R[j]) {
       A[k] = L[i];
       i++;
